f423e9ad81a3186f439958716b9883d04df3c986
AiOfLautech <babalolahephzibah2@gmail.com>
2025-04-14 17:05:04 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const mongoUrl = process.env.MONGO_URL || 'your-mongodb-connection-string';
+  const mongoUrl = process.env.MONGO_URL || 'mongodb+srv://godwinhephzibah25:kzdz0iKbfpai1ipo@cluster0.da67b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
   try {
     await mongoose.connect(mongoUrl, {
       useNewUrlParser: true,

c253d4a4282ab2038e45ceff6aae6a72ade99903
AiOfLautech <babalolahephzibah2@gmail.com>
2025-04-14 16:02:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,18 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const path = require("path");
-
-// Import API routes
-const apiRoutes = require("./routes/api");
-
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.PORT || 4000;
+const port = 3000;
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
 
-// Serve static files from project root
-app.use(express.static(path.join(__dirname)));
+const connectDB = require('./config/db');
+connectDB();
 
-app.use("/api", apiRoutes);
+const apiRoutes = require('./routes/api');
+app.use('/api', apiRoutes);
 
-// Use your Atlas URL as the default (or override with MONGODB_URI env variable)
-const mongoURI = process.env.MONGODB_URI || "mongodb+srv://godwinhephzibah25:kzdz0iKbfpai1ipo@cluster0.da67b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-mongoose
-  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected successfully."))
-  .catch((err) => console.error("MongoDB connection error:", err));
-
-app.listen(PORT, () => {
-  console.log(`API Generator backend running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });

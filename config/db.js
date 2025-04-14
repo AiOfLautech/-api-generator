0c45eab7e67955cb153b3f47bfbb141a7e664007
AiOfLautech <babalolahephzibah2@gmail.com>
2025-04-14 16:01:16 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const connectDB = async (mongoURI) => {
+const connectDB = async () => {
+  const mongoUrl = process.env.MONGO_URL || 'mongodb+srv://godwinhephzibah25:kzdz0iKbfpai1ipo@cluster0.da67b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
   try {
-    await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log("MongoDB connected (from config/db.js)");
+    await mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('MongoDB connected successfully');
   } catch (error) {
-    console.error("MongoDB connection error:", error);
+    console.error('MongoDB connection error:', error);
     process.exit(1);
   }
 };
